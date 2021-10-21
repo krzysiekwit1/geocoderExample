caa3bf1232e8362498553d884bff5dc8ffb8aabb
Krzysztof Witkowski <krzysiekwit@gmail.com>
2021-10-21 21:54:07 +0200

    added mapbox map example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,183 +1,244 @@
-import * as React from "react"
+import * as React from 'react'
+import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { useEffect } from 'react'
+import '../styles/index.css'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3J6eXNpZWt3aXQiLCJhIjoiY2t1eHhraTMwMzRvZDJwcXIxM3c1a3c1OSJ9.KBCERL0NTvMfr1t9vsmwEg'
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [13.404954, 52.520008],
+      zoom: 9
+    })
+    map.on('load', () => {
+      map.addSource('maine', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Polygon',
+            'coordinates': [
+              [
+                [
+                  13.164975,
+                  52.601712
+                ],
+                [
+                  13.153884,
+                  52.577705
+                ],
+                [
+                  13.158736,
+                  52.515311
+                ],
+                [
+                  13.19825,
+                  52.515733
+                ],
+                [
+                  13.220433,
+                  52.509405
+                ],
+                [
+                  13.20033,
+                  52.488303
+                ],
+                [
+                  13.203103,
+                  52.470991
+                ],
+                [
+                  13.19617,
+                  52.453674
+                ],
+                [
+                  13.186465,
+                  52.438885
+                ],
+                [
+                  13.206569,
+                  52.422399
+                ],
+                [
+                  13.32095,
+                  52.414789
+                ],
+                [
+                  13.374328,
+                  52.449871
+                ],
+                [
+                  13.423546,
+                  52.454096
+                ],
+                [
+                  13.4998,
+                  52.428741
+                ],
+                [
+                  13.547632,
+                  52.408446
+                ],
+                [
+                  13.576054,
+                  52.45832
+                ],
+                [
+                  13.558724,
+                  52.49168
+                ],
+                [
+                  13.65924,
+                  52.493368
+                ],
+                [
+                  13.670332,
+                  52.526279
+                ],
+                [
+                  13.670332,
+                  52.555372
+                ],
+                [
+                  13.669639,
+                  52.58276
+                ],
+                [
+                  13.604476,
+                  52.601291
+                ],
+                [
+                  13.571202,
+                  52.586972
+                ],
+                [
+                  13.542086,
+                  52.601291
+                ],
+                [
+                  13.481083,
+                  52.621076
+                ],
+                [
+                  13.419387,
+                  52.607185
+                ],
+                [
+                  13.329268,
+                  52.626968
+                ],
+                [
+                  13.292528,
+                  52.608448
+                ],
+                [
+                  13.21558,
+                  52.6135
+                ],
+                [
+                  13.164975,
+                  52.601712
+                ]
+              ]
+            ]
+          }
+        }
+      })
+    })
+    const loadColorOnPolygon = () => {
+      if (map.isStyleLoaded()) {
+        map.addLayer({
+          'id': 'maine',
+          'type': 'fill',
+          'source': 'maine', // reference the data source
+          'layout': {},
+          'paint': {
+            'fill-color': '#0080ff', // blue color fill
+            'fill-opacity': 0.5
+          }
+        })
+        map.addLayer({
+          'id': 'outline',
+          'type': 'line',
+          'source': 'maine',
+          'layout': {},
+          'paint': {
+            'line-color': '#000',
+            'line-width': 3
+          }
+        })
+
+      }
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([13.204954, 52.520008])
+        .addTo(map)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(
+              `<h3>Location 1</h3><p>headquarters</p>`
+            )
+        )
+
+      const marker2 = new mapboxgl.Marker()
+        .setLngLat([13.404954, 52.480008])
+        .addTo(map)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(
+              `<h3>Location 2</h3><p>headquarters2</p>`
+            )
+        )
+    }
+
+    const loadMarkerForGeocoder = () => {
+      if (!map.getSource('single-point-geocoder')) {
+        map.addSource('single-point-geocoder', {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: []
+          }
+        })
+        const layerGeoCoder = map.addLayer({
+          id: 'pointForGeoCoder',
+          source: 'single-point-geocoder',
+          type: 'circle',
+          paint: {
+            'circle-radius': 10,
+            'circle-color': '#448ee4'
+          }
+        })
+      }
+    }
+    map.on('data', loadColorOnPolygon)
+    map.on('data', loadMarkerForGeocoder)
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: true,
+      placeholder: 'Search for places in Berkeley',
+    })
+    geocoder.on('result', (event) => {
+      map.getSource('single-point-geocoder').setData(event.result.geometry)
+    })
+    map.addControl(geocoder)
+
+  }, [])
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <div>
+      <div id='map' style={{ height: '100vh', width: '100vw' }}>
+      </div>
+    </div>
+
   )
 }
 
